55ec865e1d6286fe477cae865fc80394cc13b8fc
Vern01 <verdunw2@gmail.com>
2016-04-14 13:37:18 +0200

    length validations test added

---
+++ check_length.c
@@ -0,0 +1,42 @@
+/*
+Check that the map lines are all the same length.
+*/
+int check_length(char **map, int y_length)
+{
+	int	length;
+	int error;
+	int	x;
+	int	y;
+	
+	y = 1;
+	error = 1;
+	length = first_length(map[0]);
+	while(y < y_length)
+	{
+		x = 0;
+		while(map[y][x] != '\n')
+			x++;
+		if(y != 0 && x != length)
+		{
+			error = 0;
+			break;
+		}
+	}
+	if(error == 0)
+		return 0;
+	else
+		return 1;
+}
+
+/*
+Get's the ref point of length that is required of other lines.
+*/
+ int first_length(char *map)
+ {
+	 int	x;
+	 
+	 x = 0;
+	 while(map[x] != '\n')
+		 x++;
+	 return x;
+ }
