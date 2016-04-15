51cce4f6a17c9d115fc729de5dee55b591551ba2
deandt100 <dean100@live.co.za>
2016-04-15 11:02:14 +0200

    added header, getMap.c, bsq.c

---
+++ bsq.c
@@ -0,0 +1,64 @@
+#include "bsg_head.h"
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	printMap(char **map, int lines)
+{
+	int	l;
+	
+	l = 0;
+	while (l <= lines)
+		ft_putstr(map[l++]);
+	
+}
+
+void	freeMap(char **map, int lines)
+{
+	int	l;
+
+	l = 0;
+	while (l <= lines)
+		free(map[l++]);
+	free(map);
+}
+
+int		main(int argv, char** argc)
+{
+	int		maps;
+	int		i;
+	int		lines;
+	char	**map;
+
+	i = 1;
+	maps = argv - 1;
+	if (maps == 0)
+	{
+		map = getMapStdIn(&lines);
+		//if (checkMap(char **map));//check if valid return 1 if no errors found
+		//		solveSquares(argc[i]); //will solve map and output solution
+		return (0);		
+	}
+	else
+	{
+		while (i <= maps)
+		{
+			map = getMapFile(argc[i], &lines); // save map into 2D array from file
+		//	if (checkMap(char **map)); // check if valid return 1 if no errors found
+		//		solveSquares(argc[i]); //will solve map and output solution
+			printMap(map, lines); //will move inro solve function
+			i++;
+			freeMap(map, lines);
+		}
+	}
+	return (0);
+}
---
+++ getMap.c
@@ -0,0 +1,88 @@
+/*Functions to retrieve map and save into char** array 
+ Note that \n characters and \0 is also saved into array
+ might remove \n in the future
+ */
+
+#include "bsg_head.h"
+
+int		countLines(char *buffer, int size)
+{
+	int	i;
+	int	lines;
+
+	i = 0;
+	lines = 0;
+	while (i < size)
+	{
+		if (buffer[i] == '\n')
+			lines++;
+		i++;
+	}
+	return (lines);
+}
+
+int		lineLength(char *buffer, int pos, int size)
+{
+	int	i;
+	int	linelen;
+
+	i = pos;
+	linelen = 0;
+	while (buffer[i] != '\n' && i < size)
+	{
+			linelen++;
+			i++;
+	}
+	return (linelen);
+}
+
+char	**createArray(char *buffer, int size, int lines)
+{
+	int		linelen;
+	int		r;
+	int		c;
+	int		i;
+	char	**map;
+
+	i = 0;
+	r = 0;
+	map = (char**)malloc(lines * sizeof(char*));
+	while (r <= countLines(buffer, size) && i < size)
+	{
+		linelen = lineLength(buffer, i , size) + 1;
+		c = 0;
+		map[r] = (char*)malloc((linelen + 1) * sizeof(char));
+		while (c < linelen)
+			map[r][c++] = buffer[i++];
+		map[r][c] = '\0';
+		r++;
+	}
+	return (map);
+}
+
+char	**getMapFile(char *fileName, int *lines)
+{
+	char	buffer[4096];
+	int		file;
+	int		size;
+
+	file = open(fileName, O_RDONLY, 1);
+	size = read(file, buffer, 4096);
+	buffer[size] = '\n';
+	buffer[size + 1] = '\0';
+	close(file);
+	*lines = countLines(buffer, size);
+	return (createArray(buffer, size, *lines));
+}
+
+char	**getMapStdIn(int *lines)
+{
+	char	buffer[4096];
+	int		size;
+
+	size = read(0, buffer, 4096);
+	buffer[size] = '\n';
+	buffer[size + 1] = '\0';
+	*lines = countLines(buffer, size);
+	return (createArray(buffer, size, *lines));
+}
